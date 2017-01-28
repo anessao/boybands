dfeb2ffcf26c482e53d5f17421d25a10dc97f640
Anessa Ortner <anessa.ortner@gmail.com>
2017-01-28 12:48:56 -0600

    removed alternate code and cleaned extra var

--- boybands.js
+++ boybands.js
@@ -1,36 +1,5 @@
-// for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
-//   // Add the band names into the correct <div>
-//   currentBand = ???;
-//   // Add the veggie names into the correct <div>
-//   currentVeggie = ???;
-// }
-
-// var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
-// var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli",  "Squash"];
-
-// var bandElement = document.getElementById("boy-bands");
-// var veggieElement = document.getElementById("vegetables");
-// var bandList = "";
-// var veggieList = "";
-
-// var bandCounter = 0;
-// var veggieCounter = 0;
-
-// for (bandCounter; bandCounter < bands.length; bandCounter++) {
-// 	bandList += "<li>" + bands[bandCounter] + "</li>"
-// }
-
-// bandElement.innerHTML = "<ul>" + bandList + "</ul>";
-
-// for (veggieCounter; veggieCounter < vegetables.length; veggieCounter++) {
-// 	veggieList +="<li>" + vegetables[veggieCounter] + "</li>"
-// }
-// veggieElement.innerHTML = "<ul>" + veggieList + "</ul>";
-
-
-//one for loop for it all?
-var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction", "something"];
-var vegetables = ["Carrots", "Kale"];
+var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli",  "Squash"];
 
 
 var bandElement = document.getElementById("boy-bands");
@@ -39,8 +8,6 @@ var bandList = "";
 var veggieList = "";
 
 counter = 0;
-bcounter = 0;
-vcounter = 0;
 var loopTracker = 5;
 
 while (loopTracker < bands.length) {
